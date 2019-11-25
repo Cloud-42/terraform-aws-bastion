5599bfce9a9a8451266aba88a8ca3fa1816e44aa
Cloud-42 <hello@cloud42.io>
2019-11-25 10:59:07 +0000

    update examples
    Update examples to include multi-AZ subnets from VPC module.

--- examples/main-inc-notification.tf
+++ examples/main-inc-notification.tf
@@ -9,7 +9,7 @@ module "bastion" {
   key_name            = "ec2_key"
   image_id            = data.aws_ami.latest_amazon_linux_ami_useast2.id
   orchestration       = "https://github.com/build"
-  vpc_zone_identifier = "subnet-xyxyxyx"
+  vpc_zone_identifier = ["${element(split(",", module.vpc.subnets_public), 0)}", "${element(split(",", module.vpc.subnets_public), 1)}"]
   security_groups     = "sg-xyxyxyxyzz"
   dns_domain_name     = "prod.domain.io"
   hosted_zone_id      = "XYXYJ44DTY4JS"
