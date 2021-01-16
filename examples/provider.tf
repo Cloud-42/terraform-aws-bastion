289a06b9a89cc85cdb7efa06a107d1895427afd5
Cloud42 <hello@cloud42.io>
2021-01-16 10:58:12 +0000

    Update example

--- examples/provider.tf
+++ examples/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  profile             = "${var.aws_profile}"
-  region              = "${var.aws_region}"
-  allowed_account_ids = ["${var.account_id}"]
+  profile             = var.aws_profile
+  region              = var.region
+  allowed_account_ids = var.account_id
 }
