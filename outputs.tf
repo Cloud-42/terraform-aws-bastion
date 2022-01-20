66ac9a8000ed6642bcdec43bf0cf153cdc0f3705
Cloud-42 <hello@cloud42.io>
2022-01-20 09:27:52 +0000

    Add name as an output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,8 @@
-#
-# Define outputs
-#
-
 output "asg_id" {
   description = "Auto Scaling Group id"
   value       = aws_autoscaling_group.bastion.id
 }
-
+output "asg_name" {
+  description = "Auto Scaling Name"
+  value       = aws_autoscaling_group.bastion.name
+}
